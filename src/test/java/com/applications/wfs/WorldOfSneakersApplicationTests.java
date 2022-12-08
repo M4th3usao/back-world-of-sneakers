d7411181c627b308b9e6a92ef0901d9a97552921
Matheus Henrique <matheus.rico17@gmail.com>
2022-12-07 21:09:26 -0300

    iniciando o back end do projeto

---
+++ src/test/java/com/applications/wfs/WorldOfSneakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applications.wfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldOfSneakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
